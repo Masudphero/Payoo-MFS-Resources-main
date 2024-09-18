a7ce61e4fa66b8360f8387ce3f8996310ecc7334
Masudphero~ <mdmasudrana8547@gmail.com>
2024-09-18 23:22:04 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,14 @@
+// console.log('button clicking file added')
+// form submit reloading the page
+
+document.getElementById('button-id'),addEventListener('click', function(event){
+//    vejal to beginners      (reloading browser)
+    event.preventDefault()  
+console.log('login button clicked');
+
+// step3.
+const phoneNumber=document.getElementById('phone-number').value;
+console.log(phoneNumber);
+
+})
+
