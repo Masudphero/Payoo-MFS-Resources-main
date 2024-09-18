16f11ac3bd57d4bdaad9e72e644a32e2ed267219
Masudphero~ <mdmasudrana8547@gmail.com>
2024-09-19 00:28:09 +0600

    add money section don

--- js/login.js
+++ js/login.js
@@ -8,7 +8,17 @@ console.log('login button clicked');
 
 // step3.
 const phoneNumber=document.getElementById('phone-number').value;
-console.log(phoneNumber);
+const pinNumber=this.document.getElementById('pin-number').value;
+
+console.log(phoneNumber,pinNumber);
+
+if(phoneNumber==='5'&& pinNumber==='1234'){
+    console.log('you are login');
+    window.location.href='/home.html';
+}
+else{
+    alert('Wrong phone number or pin');
+}
 
 })
 
