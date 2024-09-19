66dd054206393daebdb7c60cc66e5813ac5802f0
Masudphero~ <mdmasudrana8547@gmail.com>
2024-09-19 09:38:31 +0600

    add money second section

--- js/home.js
+++ js/home.js
@@ -1,1 +1,14 @@
-console.log('home cartun bangla')
+// add money to the account
+
+document.getElementById('btn-add-money'),addEventListener('click', function(event){
+    event.preventDefault();
+    console.log('maney add btn bangladesh')
+
+    // step-2 get money to be added to the account
+    const addMoneyInput=document.getElementById('inpute-add-money').value;
+    console.log(addMoneyInput)
+
+    const pinNumberInput=document.getElementById('input-pin-number').value;
+    console.log(pinNumberInput)
+    if()
+})
